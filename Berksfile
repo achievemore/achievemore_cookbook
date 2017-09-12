84a1d8faef11b59fe6bdd5c73594f5cdbc71e391
Thiago da Anunciação <thiago.anunciacao@me.com>
2017-09-12 10:21:46 -0300

    update cookbook

--- Berksfile
+++
@@ -1,6 +0,0 @@
-source 'https://supermarket.chef.io'
-
-cookbook 'opsworks_ruby', '1.7.0'
-cookbook 'packages', '~> 1.0.0'
-
-metadata
---
+++ achievemore_cookbook/recipes/default.rb
@@ -0,0 +1,15 @@
+#
+# Cookbook:: achievemore_cookbook
+# Recipe:: default
+#
+# Copyright:: 2017, The Authors, All Rights Reserved.
+
+node.default['packages-cookbook'] = [
+  'nodejs',
+  'libmysqlclient-dev',
+  'redis-server',
+  'imagemagick',
+  'memcached',
+  'language-pack-pt',
+  'monit'
+]
