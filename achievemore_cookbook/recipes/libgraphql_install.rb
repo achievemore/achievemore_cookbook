71be212fef2fdc66c59c885be9572e227f1c4c60
Thiago da Anunciação <thiago.anunciacao@me.com>
2017-09-19 18:15:28 -0300

    add recipe to install libgraphqlparser

--- achievemore_cookbook/recipes/libgraphql_install.rb
+++ achievemore_cookbook/recipes/libgraphql_install.rb
@@ -1,18 +1,26 @@
 tar_location = "#{Chef::Config['file_cache_path']}/#{node['libgraphql']['source_url']}"
+module_location = "#{Chef::Config['file_cache_path']}/libgraphql/#{node['libgraphql']['source_checksum']}"
 
 remote_file tar_location do
   source node['libgraphql']['source_url']
+  checksum node['libgraphql']['source_checksum']
+end
+
+directory module_location do
+  mode      '0755'
+  recursive true
+  action    :create
 end
 
 bash 'extract_libgraphql' do
   cwd  ::File.dirname(tar_location)
   user 'root'
   code <<-EOH
-    tar -xzvf #{::File.basename(tar_location)}
-    cd libgraphqlparser-#{node['libgraphql']['version']}/
+    tar -xzvf #{::File.basename(tar_location)} -C #{module_location}
+    cd #{module_location}/libgraphqlparser-#{node['libgraphql']['version']}/
     cmake .
     make
     make install
   EOH
-  not_if { ::File.dirname(tar_location) }
+  only_if { ::File.dirname(tar_location) }
 end
