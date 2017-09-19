318bd996b57508de664c1d007c249d2543e01e0d
Thiago da Anunciação <thiago.anunciacao@me.com>
2017-09-19 18:20:37 -0300

    add recipe to install libgraphqlparser

--- achievemore_cookbook/attributes/default.rb
+++ achievemore_cookbook/attributes/default.rb
@@ -1,3 +1,4 @@
 default['libgraphql']['version']         = '0.6.0'
 default['libgraphql']['source_checksum'] = 'ea529629f46bd992a4e1b790ab2e1e00a0ff700e'
-default['libgraphql']['source_url']      = "https://github.com/graphql/libgraphqlparser/archive/v#{default['libgraphql']['version']}.tar.gz"
+default['libgraphql']['filename'] = "v#{default['libgraphql']['version']}.tar.gz"
+default['libgraphql']['source_url']      = "https://github.com/graphql/libgraphqlparser/archive/#{default['libgraphql']['filename']}"
